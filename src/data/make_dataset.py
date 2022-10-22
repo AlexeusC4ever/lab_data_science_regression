9ecad28a2b22dd72d8fdb726cf5c7a932f00766e
AlexeusC4ever <dimabrat74@gmail.com>
2022-10-22 16:48:51 +0300

    train_val_split_and_preprocess_united

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -8,7 +8,7 @@ import pandas as pd
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 from sklearn.impute import SimpleImputer
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 import src.config as cfg
 import src.utils as utils
@@ -19,7 +19,12 @@ import src.utils as utils
 @click.argument('input_filepath_test', type=click.Path(exists=True))
 @click.argument('output_filepath_train', type=click.Path())
 @click.argument('output_filepath_test', type=click.Path())
-def main(input_filepath_train, input_filepath_test, output_filepath_train, output_filepath_test):
+@click.argument('output_filepath_train_x', type=click.Path())
+@click.argument('output_filepath_train_y', type=click.Path())
+@click.argument('output_filepath_val_x', type=click.Path())
+@click.argument('output_filepath_val_y', type=click.Path())
+def main(input_filepath_train, input_filepath_test, output_filepath_train, output_filepath_test,
+    output_filepath_train_x, output_filepath_train_y, output_filepath_val_x, output_filepath_val_y):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
@@ -39,6 +44,19 @@ def main(input_filepath_train, input_filepath_test, output_filepath_train, outpu
 
     utils.save_as_pickle(df_train, output_filepath_train)
     utils.save_as_pickle(df_test, output_filepath_test)
+
+
+    #train_val_split
+
+    X, y = df_train[cfg.FEATURES], df_train[cfg.TARGET]
+
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+    utils.save_as_pickle(x_train, output_filepath_train_x)
+    utils.save_as_pickle(y_train, output_filepath_train_y)
+    utils.save_as_pickle(x_test, output_filepath_val_x)
+    utils.save_as_pickle(y_test, output_filepath_val_y)
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
